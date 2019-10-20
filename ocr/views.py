5b68441395ace82f1b089f2250c84f2e40253f2d
anarchistMegaByte <manavbharambe93@gmail.com>
2019-10-21 00:04:45 +0530

    feat(ocr views): algos for string matching

--- ocr/views.py
+++ ocr/views.py
@@ -8,14 +8,26 @@ import json
 # Create your views here.
 @csrf_exempt
 def test_score(request):
+	from fuzzywuzzy import fuzz 
+	from fuzzywuzzy import process 
 	result_dict = DEFAULT_RESPONSE_DICT.copy()
 	try:
 		if request.method == 'POST':
 			data=json.loads(request.body)
-			print(data["student_answer"])
-			print(data["ideal_answer"])
+			s_ans = data["student_answer"]
+			i_ans = data["ideal_answer"]
+
+			ratio_algorithm = fuzz.ratio(s_ans, i_ans)
+			token_sort_ratio_algorithm = fuzz.token_sort_ratio(s_ans, i_ans)
+			token_set_ratio_algorithm = fuzz.token_set_ratio(s_ans, i_ans)
+			partial_ratio_algorithm = fuzz.partial_ratio(s_ans, i_ans)
+
 			test_Score = {}
-			test_Score["algo1"] = 0
+			test_Score["ratio_algorithm"] = ratio_algorithm
+			test_Score["token_sort_ratio_algorithm"] = token_sort_ratio_algorithm
+			test_Score["token_set_ratio_algorithm"] = token_set_ratio_algorithm
+			test_Score["partial_ratio_algorithm"] = partial_ratio_algorithm
+
 			result_dict["data"] = test_Score
 			result_dict["status"] = True
 			result_dict["message"] = "Success"
